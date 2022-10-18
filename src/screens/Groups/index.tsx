49dd026c7c4f684e1d537eaff8b484c0a4ecee11
amanda-santos <amandasf0200@gmail.com>
2022-10-18 18:53:30 -0300

    feat: ✨ add Highlight component

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,12 +1,14 @@
 import { ReactElement } from "react";
 
 import { Header } from "@components/Header";
+import { Highlight } from "@components/Highlight";
 import { Container } from "./styles";
 
 export const Groups = (): ReactElement => {
   return (
     <Container>
       <Header showBackButton />
+      <Highlight title="Turmas" subtitle="jogue com a sua turma" />
     </Container>
   );
 };
