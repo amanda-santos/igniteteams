a3b6ac9dc502eb269dc0234aabf80f231ea39148
amanda-santos <amandasf0200@gmail.com>
2022-10-25 18:39:59 -0300

    refactor: ♻️ add css helper

--- App.tsx
+++ App.tsx
@@ -21,7 +21,7 @@ export default function App() {
           backgroundColor="transparent"
           translucent
         />
-        {fontsLoaded ? <NewGroup /> : <LoadIndicator />}
+        {fontsLoaded ? <Groups /> : <LoadIndicator />}
       </>
     </ThemeProvider>
   );
